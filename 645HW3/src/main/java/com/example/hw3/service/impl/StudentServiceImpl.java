0dd8c6ff37e325a2b591c75f9e94ccafb7a0f9cc
Meghana Kancherla <meghana.kancherla2911@gmail.com>
2023-07-28 14:23:44 -0400

    Updates sprintboot application location

---
+++ 645HW3/src/main/java/com/example/hw3/service/impl/StudentServiceImpl.java
@@ -0,0 +1,77 @@
+package com.example.hw3.service.impl;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.example.hw3.exception.ResourceNotFoundException;
+import com.example.hw3.model.Student;
+import com.example.hw3.repository.StudentRepository;
+import com.example.hw3.service.StudentService;
+
+@Service
+public class StudentServiceImpl implements StudentService{
+
+	public StudentRepository studentRepository;
+	
+	
+	public StudentServiceImpl(StudentRepository studentRepository) {
+		super();
+		this.studentRepository = studentRepository;
+	}
+
+	@Override
+	public Student saveStudent(Student student) {
+		return studentRepository.save(student);
+	}
+
+	@Override
+	public List<Student> getAllStudents() {
+		return studentRepository.findAll();
+	}
+
+	@Override
+	public Student getStudentById(long id) {
+		Optional<Student> student = studentRepository.findById(id);
+		
+		if (student.isPresent()) {
+			return student.get();
+		}
+		else {
+			throw new ResourceNotFoundException("Student", "ID", id);
+		}
+	}
+
+	@Override
+	public Student updateStudent(Student student, long id) {
+		Student existingStudent = studentRepository.findById(id).orElseThrow(
+				() -> new ResourceNotFoundException("student", "Id", id));
+		existingStudent.setFirstName(student.getFirstName());
+		existingStudent.setLastName(student.getLastName());
+		existingStudent.setStreetAddress(student.getStreetAddress());
+		existingStudent.setCity(student.getCity());
+		existingStudent.setState(student.getState());
+		existingStudent.setZip(student.getZip());
+		existingStudent.setPhoneNumber(student.getPhoneNumber());
+		existingStudent.setEmail(student.getEmail());
+		existingStudent.setDateOfSurvey(student.getDateOfSurvey());
+		existingStudent.setLikeCampus(student.getLikeCampus());
+		existingStudent.setInterest(student.getInterest());
+		existingStudent.setRecommend(student.getRecommend());;
+		existingStudent.setRaffle(student.getRaffle());
+		existingStudent.setComments(student.getComments());
+		
+		studentRepository.save(existingStudent);
+		
+		return existingStudent;
+	}
+
+	@Override
+	public void deleteStudent(long id) {
+		studentRepository.findById(id).orElseThrow(()->new ResourceNotFoundException("Student", "Id", id));
+		
+		studentRepository.deleteById(id);
+	}
+
+}
--- src/main/java/com/example/hw3/repository/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.hw3.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.hw3.model.Student;
-
-public interface StudentRepository extends JpaRepository<Student, Long>{
-	
-}
