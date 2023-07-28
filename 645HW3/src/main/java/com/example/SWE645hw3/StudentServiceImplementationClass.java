05116bc4bf8914be3de173a2fc8d03aad734a3b6
Meghana Kancherla <meghana.kancherla2911@gmail.com>
2023-07-28 18:22:03 -0400

    Changed the folder name

--- 645HW3/src/main/java/com/example/SWE645hw3/StudentServiceImplementationClass.java
+++ 645HW3/src/main/java/com/example/SWE645hw3/StudentServiceImplementationClass.java
@@ -6,12 +6,12 @@ import java.util.Optional;
 import org.springframework.stereotype.Service;
 
 @Service
-public class StudentServiceImplementation implements StudentServiceClass{
+public class StudentServiceImplementationClass implements StudentServiceClass{
 
 	public StudentRepositoryClass studentRepository;
 	
 	
-	public StudentServiceImplementation(StudentRepositoryClass studentRepository) {
+	public StudentServiceImplementationClass(StudentRepositoryClass studentRepository) {
 		super();
 		this.studentRepository = studentRepository;
 	}
